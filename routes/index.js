d83b21318c718817adf7f05734b089d253e08897
Chris Quinn <chrisq@ultra.me>
2019-06-29 21:14:18 -0600

    Doint cool things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
+  res.render('index', {page:'', menuId:'home'});
 });
 
 router.get('/about', function(req, res, next) {
